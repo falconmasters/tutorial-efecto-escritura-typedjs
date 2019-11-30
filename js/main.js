de11d01b799155561b1ba1e04cf59488d240d810
Carlos Arturo <carlos@falconmasters.com>
2019-11-29 19:15:05 -0600

    Archivos finales

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="mascota">Gato</i>',
+		'<i class="mascota">Perro</i>',
+		'<i class="mascota">Conejo</i>',
+		'<i class="mascota">Pez</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
